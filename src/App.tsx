e33d95dae51bc3010f41b2d86f1b659b08f9fc71
fellinger-schule <fellinger-schule@users.noreply.github.com>
2021-01-21 12:22:47 +0100

    added tab system

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function TabContent(props:any) {
+	switch(props.tabName) {
+		case "kassa": {
+			return <h1>hallo bei der kassa</h1>;
+			break;
+		}
+		case "kundencenter": {
+			return <h1>kundencenter</h1>;
+			break;
+		}
+		default:
+			return <h1>???</h1>;
+			break;
+	}
+}
+
 function App() {
+
+	const [activeTab, setActiveTab] = useState("kassa");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+	 <div className="App">
+		<ul className="nav nav-pills nav-fill">
+		<li className="nav-item">
+			<a className={`nav-link ${activeTab == "kassa" ? "active": ""}`} onClick={() => setActiveTab("kassa")} href="#">Kassa</a>
+		</li>
+		<li className="nav-item">
+			<a className={`nav-link ${activeTab == "kundencenter" ? "active": ""}`} onClick={() => setActiveTab("kundencenter")} href="#">Kundencenter</a>
+		</li>
+		<li className="nav-item">
+			<a className="nav-link" href="#">Link</a>
+		</li>
+	 </ul>
+	 <TabContent tabName={activeTab} />
+	 
+	 </div>
   );
 }
 
